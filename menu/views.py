7622965d1196c71036753ec4258dc6362b033ff4
Cole Hoover <colehoov1@gmmail.com>
2021-03-03 16:28:19 -0600

    Fixing logout on product listing and employee detail views.

--- menu/views.py
+++ menu/views.py
@@ -6,6 +6,7 @@ from .forms import logout
 from Kiosk.models import Employee
 from Kiosk.models import Active_Employee
 import time
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -25,7 +26,7 @@ def index(request):
             employee_id = employee.employee_id
 
             role = employee.role
-            index.employee_info = {'first_name' : first_name, 'last_name' : last_name, 'employee_id' : employee_id, 'role' : role}
+            employee_info = {'first_name' : first_name, 'last_name' : last_name, 'employee_id' : employee_id, 'role' : role}
     except:
         pass
 
@@ -61,13 +62,72 @@ def index(request):
 
             
     if auth:
-        return render(request, 'index_menu.html', index.employee_info)
+        return render(request, 'index_menu.html', employee_info)
     else:
         return HttpResponseRedirect('/login')
         
-
+@csrf_exempt
 def productListing(request):
-    return render(request, 'productListing.html', index.employee_info)
+    auth = False
+    try:
+        session_key = request.session['session_key']
+        if session_key:    
+            auth = Active_Employee.objects.filter(session_key=session_key).first()
+            employee = Employee.objects.filter(employee_id=auth.employee_id).first()
+            first_name = employee.first_name
+            last_name = employee.last_name
 
+            employee_id = employee.employee_id
+
+            role = employee.role
+            employee_info = {'first_name' : first_name, 'last_name' : last_name, 'employee_id' : employee_id, 'role' : role}
+    except:
+        pass
+
+    if request.method == 'POST':
+         if 'logout_click' in request.POST:
+            try:
+                del request.session['session_key']
+                employee.active = False
+                auth.delete()
+                auth = False
+            except:
+                pass
+    if auth:
+       return render(request, 'productListing.html', employee_info)
+    else:
+        return HttpResponseRedirect('/login')
+    
+
+@csrf_exempt
 def employeeDetail(request):
-    return render(request, 'employeeDetail.html', index.employee_info)
+    auth = False
+    try:
+        session_key = request.session['session_key']
+        if session_key:    
+            auth = Active_Employee.objects.filter(session_key=session_key).first()
+            employee = Employee.objects.filter(employee_id=auth.employee_id).first()
+            first_name = employee.first_name
+            last_name = employee.last_name
+
+            employee_id = employee.employee_id
+
+            role = employee.role
+            employee_info = {'first_name' : first_name, 'last_name' : last_name, 'employee_id' : employee_id, 'role' : role}
+    except:
+        pass
+
+    if request.method == 'POST':
+         if 'logout_click' in request.POST:
+            try:
+                del request.session['session_key']
+                employee.active = False
+                auth.delete()
+                auth = False
+            except:
+                pass
+    if auth:
+       return render(request, 'employeeDetail.html', employee_info)
+    else:
+        return HttpResponseRedirect('/login')
+    

a85b5652ccb4be4530c1889dd8a4b3bff3e29d39
andre3 <andfnts@gmail.com>
2021-04-01 08:50:35 -0500

    testing new database schema

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    
+    # Django auto increment primary key
+    record_id = models.AutoField()
+
+    item_id = models.CharField(max_length=5, primary_key=True) 
+
+    item_name = models.TextField()
+
+    item_price = models.DecimalField(decimal_places=2)
+
+    item_description = models.TextField()
+
+    # Django auto timestamp creation of user (doens't update upon login)
+    created_on = models.DateField(auto_now_add=True)
